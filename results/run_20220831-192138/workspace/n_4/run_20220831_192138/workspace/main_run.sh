1d5fe6083173519b74bba1c6f7197803b3aba3b5
p1003 <plgpitrus@login01.ares.cyfronet.pl>
2022-08-31 19:28:00 +0200

    Getting endtime of collect.sh

--- results/run_20220831-192138/workspace/n_4/run_20220831_192138/workspace/main_run.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# Exit immediately if a simple command exits with a non-zero status.
-set -e
-
-# no user options provided
-
-# Run individual jobs
-/net/people/plgrid/plgpitrus/pracownia-problemowa/results/run_20220831-192138/workspace/n_4/run_20220831_192138/workspace/job_`printf %04d $SLURM_ARRAY_TASK_ID`/run.sh
