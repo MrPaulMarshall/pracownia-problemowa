d94e1e35a49163c690d52a7b4270c99f0a832c3c
Pawel Marszal <plgpmarszal@login01.ares.cyfronet.pl>
2022-09-09 18:53:55 +0200

    Generate plot with logarithmix X-axis

--- scripts/plot.py
+++ scripts/plot.py
@@ -30,29 +30,53 @@ def optimize(x, y):
 def draw_plot(f, x, y, path, primaries):
     import numpy as np
     import matplotlib.pyplot as plt
+    from matplotlib.scale import LogScale
 
     # generate prediction line
     fx = np.linspace(x[0], x[-1], 100)
     fy = f(fx)
 
     # plot prediction line and actual measurments
-    plt.title(f'Execution time for {primaries} particles')
-    plt.xlabel('Number of CPUs')
-    plt.ylabel('Execution time [s]')
+    fig, (ax_lin, ax_log) = plt.subplots(2, 1)
+
+    # linear X-axis
+    fig.set_figheight(2 * fig.get_figheight())
+
+    ax_lin.set_title(f'Execution time for {primaries} particles (linear scale)')
+    ax_lin.set_xlabel('Number of CPUs')
+    ax_lin.set_ylabel('Execution time [s]')
+
+    ax_lin.grid(visible=True, linestyle='--')
 
     if x[-1] > 8:
         from math import log, ceil
         max = 2 ** ceil(log(x[-1], 2))
         step = max // 8
-        plt.xticks(np.arange(step, max+1, step))
+        ax_lin.set_xticks(np.arange(0, max+1, step))
     else:
-        plt.xticks(np.arange(1, x[-1]+1, 1))
+        ax_lin.set_xticks(np.arange(0, x[-1]+1, 1))
 
-    plt.plot(fx, fy, linewidth=1.0, linestyle='--', label='Prediction line')
-    plt.scatter(x, y, c='#ff0000', label='Measurments')
+    ax_lin.plot(fx, fy, linewidth=1.0, linestyle='--', label='Prediction line')
+    ax_lin.scatter(x, y, c='#ff0000', label='Measurments')
 
-    plt.legend()
-    plt.savefig(path)
+    ax_lin.legend()
+
+    # logarithmix X-axis
+    ax_log.set_title(f'Execution time for {primaries} particles (log-2 scale)')
+    ax_log.set_xlabel('Number of CPUs')
+    ax_log.set_ylabel('Execution time [s]')
+
+    ax_log.grid(visible=True, linestyle='--')
+
+    ax_log.set_xscale(LogScale(ax_log.xaxis, base=2))
+
+    ax_log.plot(fx, fy, linewidth=1.0, linestyle='--', label='Prediction line')
+    ax_log.scatter(x, y, c='#ff0000', label='Measurments')
+
+    ax_log.legend()
+
+    # savefig
+    fig.savefig(path)
 
 def main(csv_path, image_path, primaries):
     import numpy as np
